7360c81cdcc4ca42c9b2742297b76f9045882367
acheung12 <ashtonbc2022@student.cis.edu.hk>
2021-05-25 18:44:43 +0800

    Things

--- app/src/main/java/com/example/iasubstituteteacher/Jobs/AcceptedJobs.java
+++ app/src/main/java/com/example/iasubstituteteacher/Jobs/AcceptedJobs.java
@@ -5,15 +5,31 @@ import java.util.ArrayList;
 public class AcceptedJobs extends Jobs
 {
 
+    String acceptorsEmail;
+
     public AcceptedJobs(String jobsId, String subject, String date, String time, String location,
-                    boolean active, String lessonPlan, String userId, String usersEmail)
+                    boolean active, String lessonPlan, String userId, String usersEmail,
+                        String acceptorsEmail)
     {
         super(jobsId, subject, date, time, location, active, lessonPlan, userId, usersEmail);
+    }
+
+    public AcceptedJobs(String acceptorsEmail) {
+        this.acceptorsEmail = acceptorsEmail;
     }
 
     public AcceptedJobs()
     {
 
     }
+
+    public String getAcceptorsEmail() {
+        return acceptorsEmail;
+    }
+
+    public void setAcceptorsEmail(String acceptorsEmail) {
+        this.acceptorsEmail = acceptorsEmail;
+    }
+
 
 }
